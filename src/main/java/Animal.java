5c90183a21cc2bcbb5e4370657c757d4d659d941
Andreas Tali <taliandreas@gmail.com>
2020-06-14 14:36:28 +0300

    used it to make inheritance exercise 2

---
+++ src/main/java/Animal.java
@@ -0,0 +1,72 @@
+public class Animal {
+    private String name;
+    private Integer age;
+    private String gender;
+    private String race;
+    private Integer weight;
+
+    public Animal() {
+    }
+
+    public Animal(String gender, String race) {
+        this("default",0,"default","default",0);
+        this.gender = gender;
+        this.race = race;
+    }
+
+    public Animal(String name, Integer age, String gender, String race, Integer weight) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.race = race;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        if (age>0){
+            this.age = age;
+        } else {
+            System.out.println(this.getName()+" age cannot be less than 0");
+        }
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        if (weight>0){
+            this.weight = weight;
+        }else {
+            System.out.println(this.getName()+" cannot weight less than 0");
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Dog dog = new Dog("male","dog");
+        Dog dog = new Dog();
         System.out.println(dog.toString());
 
     }
