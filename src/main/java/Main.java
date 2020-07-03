bbfe6b0b494039ee1879e20e9c01fdea608f2faa
Andreas Tali <taliandreas@gmail.com>
2020-07-03 16:06:53 +0300

    started prefix calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
+/**Operators: + - * /
+    Postfix Examples: 1 2 +
+    Postfix Examples: 5 15 /
+    Prefix Examples: - 2 4
+    Prefix Examples: * 11 2
+    # Postfix prefix calculator
+    Use colors in the commandline prints(B) (Google it)
+    Menu: 0-Choose between postfix prefix calculations
+    Menu: 1-Save the result in a file(B) 2-Print the result on screen
+    Menu: 3-Read from the file(B)
+    Hints: 1- You should not use hardcoded nested if-else statements for choosing between operators.
+    Hints: 2- You have to handle exceptions
+    Hints: 3- Try to implement it with usage of generic types
+ */
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to calculator.\nWhich calculator do you wish to use? (enter corresponding number)\n" +
+                "1. "+RED+"Prefix"+RESET+" calculator ("+YELLOW+"iterator "+BLUE+"number number"+RESET+")"+
+                "\n2."+RED+" Postfix"+RESET+" calculator ("+BLUE+"number number"+YELLOW+" iterator"+RESET+")");
+        switch (scanner.next()){
+            case "1":
+                System.out.println("Enter your iterator (+, -, *, /) and numbers.("+YELLOW+"iterator "+BLUE+"number number"+RESET+")");
+                //prefix.java
+                break;
+            case "2":
+                //postfix.java
+                break;
+            default:
+                System.out.println("Error! Wrong input.");
+        }
 
     }
 }
---
+++ src/main/java/Prefix.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Prefix {
+    private String prefixClac(String input){
+        String[] maths = input.split(" ");
+        return "yeet";
+    }
+}
