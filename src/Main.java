2f8ff6a1fb91f5d5b6560c1235a59b6bd00c564a
Andreas Tali <taliandreas@gmail.com>
2020-07-12 11:52:36 +0300

    empty master

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Pocket wallet = new Pocket(1000);
 
-        System.out.println(wallet.getMoney(300));
-
-        wallet.setMoney(-10);
-
-        wallet.setMoney(2000);
-
-        System.out.println(wallet.getMoney());
-
-        wallet.setMoney(5);
-
-        System.out.println(wallet.getMoney(50));
     }
 }
